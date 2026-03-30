abfa53253a47c3eeb05d82b61e71890b9675473f
airon <aironmoe_oquindo.it@wilcon.com.ph>
2026-03-30 13:39:40 +0800

    final this day

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -66,25 +66,8 @@ const isZoomed = ref(false);
       <div class="card">
         <h2>Skills</h2>
         <h4 class="fb-name">
-          Junior RPA Developer at Wilcon Depot Inc. from (August 29, 2024 to Present)
+          JavaScript, Vue.js, PHP, Laravel, MySQL, RAX, Uipath,
         </h4>
-        <p>
-          Junior RPA Developer I am currently working at Wilcon Depot Inc. as a Robotic
-          Process Automation (RPA)Developer, where I am responsible for developing,
-          maintaining, and improving their automation processes using RAX and N8N. My role
-          involves streamliningrepetitive asks of other business aaunits, managing
-          automation-related data, and supporting business efficiency through intelligent
-          automation solutions
-        </p>
-        <h4 class="fb-name">
-          Fatima University Medical Center(February 5 2024 - April 5 2024 )
-        </h4>
-        <p>
-          System Developer Carried on developing a web-based inventory management system
-          using PHP and MySQL, which involved designing and implementing database
-          structures,creating dynamic user interfaces,optimizing application performance
-          and ensuring system reliability and data accuracy throughout development process
-        </p>
       </div>
 
       <div class="card">
